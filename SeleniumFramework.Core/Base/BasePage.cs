73b323f5bd034dce292d9ab52729f144b8d3e09f
NikolasAristidou <124390585+NikolasAristidou@users.noreply.github.com>
2024-06-14 19:45:17 +0300

    Added more functionality to basepage class

--- SeleniumFramework.Core/Base/BasePage.cs
+++ SeleniumFramework.Core/Base/BasePage.cs
@@ -30,12 +30,52 @@ namespace SeleniumFramework.Core.Base
 
         public abstract string Url { get; }
 
-        public virtual void Open()
+        public virtual void GoToUrl()
         {
             Driver.Navigate().GoToUrl(ConfigReader.GetAppSetting("BaseUrl") + Url);
         }
+        public bool ValidateLogo(string elementType, string elementName)
+        {
+            try
+            {
+                IWebElement element = null;
 
-        public abstract bool IsAt();
+                switch (elementType.ToLower())
+                {
+                    case "id":
+                        element = Driver.FindElement(By.Id(elementName));
+                        break;
+                    case "class":
+                        element = Driver.FindElement(By.ClassName(elementName));
+                        break;
+                    case "name":
+                        element = Driver.FindElement(By.Name(elementName));
+                        break;
+                    case "css":
+                        element = Driver.FindElement(By.CssSelector(elementName));
+                        break;
+                    case "xpath":
+                        element = Driver.FindElement(By.XPath(elementName));
+                        break;
+                    case "tag":
+                        element = Driver.FindElement(By.TagName(elementName));
+                        break;
+                    case "link":
+                        element = Driver.FindElement(By.LinkText(elementName));
+                        break;
+                    case "partiallink":
+                        element = Driver.FindElement(By.PartialLinkText(elementName));
+                        break;
+                    default:
+                        throw new ArgumentException($"Unknown element type: {elementType}");
+                }
 
+                return element.Displayed;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
     }
 }
