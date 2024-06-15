73a5ade146cf82970e3e17d6062406d8aad8a415
NikolasAristidou <124390585+NikolasAristidou@users.noreply.github.com>
2024-06-15 15:03:29 +0300

    Added login functionality test

--- SeleniumFramework.Tests/TestClasses/Tests.cs
+++ SeleniumFramework.Tests/TestClasses/Tests.cs
@@ -4,14 +4,14 @@ using SeleniumFramework.Core.Base;
 using SeleniumFramework.Core.Utilities;
 using SeleniumFramework.Core.Enums;
 
-
 namespace SeleniumFramework.Tests.TestClasses
 {
-    [TestFixture, Order(1)]
+    [TestFixture]
     public class Tests
     {
         private IWebDriver _driver;
         private HomePage _homePage;
+        private LoginPage _loginPage;
 
         [OneTimeSetUp]
         public void OneTimeSetup()
@@ -19,20 +19,32 @@ namespace SeleniumFramework.Tests.TestClasses
             _driver = WebDriverFactory.GetWebDriver();
             _homePage = new HomePage(_driver);
             _homePage.GoToUrl();
+            _loginPage = new LoginPage(_driver);
         }
 
         [Test, Description("Verifies amazon home page loaded by validating persisting logo on the left side")]
-        public void HomePageLoadTest()
+        public void VerifyHomePageLoad()
         {
             bool isLogoDisplayed = _homePage.ValidateElementVisibility(LocatorType.Id, "nav-logo-sprites");
-            Assert.That(isLogoDisplayed, "The Amazon logo is displayed on the home page - page loaded");
+            Assert.That(isLogoDisplayed);
             Logger.Log("Home Page loaded");
         }
 
-        [Test, Description("Verifies Navigation has loaded sucessfully")]
-        public void VerifyNavigationLinkIsDisplayed()
+        [Test, Description("Verifies Navigation bar is displayed")]
+        public void VerifyNavigationBarIsDisplayed()
         {
-            Assert.That(_homePage.ValidateElementVisibility(LocatorType.Id, "nav-xshop"));
+            bool isNavBarVisible = _homePage.ValidateElementVisibility(LocatorType.Id, "nav-xshop");
+            Assert.That(isNavBarVisible);
+        }
+
+        [Test, Description("Verify User logged in successfully")]
+        public void VerifyUserLogin()
+        {
+            string nonLoggedInValue = _homePage.GetElementText(LocatorType.Id, "nav-link-accountList-nav-line-1");
+            _homePage.NavigateToLoginPage();
+            _loginPage.Login();
+            string username = _homePage.GetElementText(LocatorType.Id, "nav-link-accountList-nav-line-1");
+            Assert.That(nonLoggedInValue, Is.Not.EqualTo(username));
         }
 
         [OneTimeTearDown]
