26fb993dad4dcbb0fac1f2c303b1eb0d49bea023
NikolasAristidou <124390585+NikolasAristidou@users.noreply.github.com>
2024-06-16 19:57:59 +0300

    creditcard flow

--- SeleniumFramework.Tests/TestClasses/HomePageTests.cs
+++ SeleniumFramework.Tests/TestClasses/HomePageTests.cs
@@ -4,7 +4,6 @@ using SeleniumFramework.Core.Base;
 using SeleniumFramework.Core.Utilities;
 using SeleniumFramework.Core.Enums;
 
-
 namespace SeleniumFramework.Tests.TestClasses
 {
     [TestFixture]
@@ -21,7 +20,7 @@ namespace SeleniumFramework.Tests.TestClasses
             _homePage.GoToUrl();
         }
 
-        [Test, Description("Verifies amazon home page loaded by validating persisting logo on the left side")]
+        [Test, Description("Verifies amazon home page loaded by validating persisting logo on the left side"), Order(1)]
         public void HomePageLoadTest()
         {
             bool isLogoDisplayed = _homePage.ValidateElementVisibility(LocatorType.Id, "nav-logo-sprites");
@@ -29,16 +28,26 @@ namespace SeleniumFramework.Tests.TestClasses
             Logger.Log("Home Page loaded");
         }
 
-        [Test, Description("Verifies Navigation has loaded sucessfully")]
+        [Test, Description("Verifies Navigation has loaded successfully"), Order(2)]
         public void VerifyNavigationLinkIsDisplayed()
         {
             Assert.That(_homePage.ValidateElementVisibility(LocatorType.Id, "nav-xshop"));
+        }
+
+        [Test, Description("Verify item price card"), Order(3)]
+        public void TestMenuValues()
+        {
+            _homePage.ChooseMainMenuCategory();
+            _homePage.RetrieveMenuItem();
+            _homePage.CreditCardFlowCheck();
+            //_homePage.GetMenuMapping();
         }
 
         [OneTimeTearDown]
         public void Teardown()
         {
             _driver.Quit();
+            _driver.Dispose();
         }
     }
 }
